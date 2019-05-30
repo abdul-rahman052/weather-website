eff3d9600724df21d9030c21556ff81aae170610
Abdul Rahman -X (abdulra - WIPRO LIMITED at Cisco) <abdulra@partnet.cisco.com>
2019-05-30 17:18:50 +0530

    "Setup app for heroku"

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const hbs = require("hbs");
 const geocode=require("./utils/geocode");
 const forecast=require("./utils/forecast");
 
-app.listen(3000,()=>{
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
     console.log("Server runnning at port 3000");
 })
 
